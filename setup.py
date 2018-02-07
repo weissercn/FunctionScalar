926b613ae12fb951564bb0d0718c82ff7e875adb
weissercn <weissercn@gmail.com>
2018-02-07 17:21:54 -0500

    pypi new version

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='FunctionScaler',
-      version='0.0',
+      version='0.1',
       description='Given a discrete probability density function learn a function that turns samples from the pdf into samples from desired distribution.',
       url='https://github.com/weissercn/FunctionScaler',
       author='Constantin Weisser',
