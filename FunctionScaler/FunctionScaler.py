25dc7270882ff84d9d88efa3db1a8aae44119b5b
weissercn <weissercn@gmail.com>
2018-02-07 17:21:11 -0500

    now working

--- FunctionScaler/FunctionScaler.py
+++ FunctionScaler/FunctionScaler.py
@@ -11,26 +11,52 @@ class TransformedFunction:
 
 
 class TransformedFunction_Uniform(TransformedFunction):
-        
+
     def inv_cdf(self, n_points=100):
         inv_cdf_points = np.array(range(1,1+n_points))/(1.+n_points)
         return inv_cdf_points
 
 
 class TransformedFunction_Gauss(TransformedFunction):
-        
+
     def inv_cdf(self, n_points=100):
         # ppf is the inverse of cdf as shown by
         # norm.cdf(norm.ppf(0.95)) = 0.95
         # scipy.stats.norm.ppf = scipy.special.ndtri
         inv_cdf_points = norm.ppf( np.array(range(1,1+n_points))/(1.+n_points)  )
         return inv_cdf_points
-        
+
 def name_to_TransformedFunction(name):
     if name=="gauss" or name=="normal": return TransformedFunction_Gauss()
     elif name=="unif" or name=="uniform": return TransformedFunction_Uniform()
 
-class FunctionScalar:
+
+def unique_mask(data):
+    # data has to be sorted
+    # when we get duplicates, use the "middle one"
+    mask = np.zeros_like(data)
+    last = np.inf
+    n_same = 0 #start counting from 0
+    duplicates_found = False
+    for i, d in enumerate(data):
+        if d == last:
+            n_same += 1
+            duplicates_found = True
+        else:
+            if duplicates_found:
+                if n_same %2==0: i_dupl_used =     n_same/2
+                else:            i_dupl_used = int(n_same/2) + np.random.choice(2)
+                print "i_dupl_used : ", i_dupl_used
+                for n in range(n_same+1):
+                    mask[i-n-1] =1
+                mask[i-i_dupl_used-1] = 0
+                n_same = 0
+                duplicates_found = False
+        last = d
+
+    return mask
+
+class FunctionScaler:
     def __init__(self, aTransformedFunction):
         if isinstance(aTransformedFunction, TransformedFunction):
             self.aTransformedFunction = aTransformedFunction
@@ -38,37 +64,123 @@ class FunctionScalar:
             self.aTransformedFunction = name_to_TransformedFunction(aTransformedFunction)
         else:
             self.aTransformedFunction = aTransformedFunction
-        
+
 
     def fit(self, data):
         data = np.array(data)
+        if data.ndim ==1 : data =data.reshape(-1,1)
+
         n_points = data.shape[0]
-        #n_dims   = data.shape[1]
-        #np.atleast_2d(np.array([1,2])).shape       
-        self.LearnedFunctions = [] 
+        self.n_feats   = data.shape[1]
+
+        self.LearnedFunctions = []
         self.LearnedInvFunctions = []
 
-
-        data = np.sort(data)
         if isinstance(self.aTransformedFunction, TransformedFunction):
             y = self.aTransformedFunction.inv_cdf(n_points)
         else:
-            y = np.array(self.aTransformedFunction)     
+            y = np.array(self.aTransformedFunction)
 
-        #for d in n_dims:
+        for i_feats in range(self.n_feats):
 
-        self.LearnedFunctions.append(interp1d( data , y, kind='linear', fill_value="extrapolate"))
-        self.LearnedInvFunctions.append(interp1d( y, data, kind='linear', fill_value="extrapolate"))
+            data_1D = np.sort(data[:,i_feats])
 
+            #data_1D_no_duplicates, y_no_duplicates = remove_duplicates(data_1D, y)
+            #self.LearnedFunctions.append(interp1d( data_1D_no_duplicates , y_no_duplicates, kind='linear', fill_value="extrapolate"))
+            #self.LearnedInvFunctions.append(interp1d( y_no_duplicates, data_1D_no_duplicates, kind='linear', fill_value="extrapolate"))
+            self.makeLearnedFunctions(data_1D, y)
+
+    def makeLearnedFunctions(self, data, y):
+        #self.makeLearnedFunctions_mask(data_1D, y)        
+        # data has to be sorted
+        # when we get duplicates, use the "middle one"
+        data_no_duplicates, y_no_duplicates = [], []
+
+
+        if data[0] == data[-1]:
+            self.LearnedFunctions.append(    lambda arg: arg)
+            self.LearnedInvFunctions.append( lambda arg: arg)
+
+        else:
+
+            mask = np.zeros_like(data)
+            last = np.inf
+            n_same = 0 #start counting from 0
+            duplicates_found = False
+            for i, d in enumerate(data):
+                if d == last:
+                    test = np.inf
+                    try: test = data[i-2]
+                    except: pass
+                    if test != np.inf:
+                        data_no_duplicates = data_no_duplicates[:-1]
+                        data_no_duplicates.append( 0.999*data[i] + 0.001*data[i-1])
+
+                    n_same += 1
+                    duplicates_found = True
+                else:
+                    if duplicates_found:
+                        d_next = np.inf
+                        try: d_next = data[i+1]
+                        except: pass
+                        print "test"
+                        if last != np.inf:
+                            data_no_duplicates.append(0.999*data[i] + 0.001*data[i-1] )
+                            y_no_duplicates.append(y[i])
+                        else:
+                            data_no_duplicates.append(data[i])
+                            y_no_duplicates.append(y[i])
+                        n_same = 0
+                        duplicates_found = False
+                    else:
+                        data_no_duplicates.append(data[i])
+                        y_no_duplicates.append(y[i])
+                last = d
+
+
+            self.LearnedFunctions.append(interp1d( data_no_duplicates , y_no_duplicates, kind='linear', fill_value="extrapolate"))
+            self.LearnedInvFunctions.append(interp1d( y_no_duplicates, data_no_duplicates, kind='linear', fill_value="extrapolate"))
+
+
+
+
+    def makeLearnedFunctions_mask(self, data_1D, y):
+        data_1D_mask = unique_mask(data_1D)
+        data_1D_no_duplicates, y_no_duplicates = (  np.ma.compressed(np.ma.array(data_1D, mask=data_1D_mask))   ,  np.ma.compressed(np.ma.array(y, mask=data_1D_mask)) )
+        self.LearnedFunctions.append(interp1d( data_1D_no_duplicates , y_no_duplicates, kind='linear', fill_value="extrapolate"))
+        self.LearnedInvFunctions.append(interp1d( y_no_duplicates, data_1D_no_duplicates, kind='linear', fill_value="extrapolate"))
 
     def transform(self, data):
-        return self.LearnedFunctions[0](data)
+        data = np.array(data)
+        if data.ndim ==1 : data = data.reshape(-1,1)
 
-    def fittransform(self, data):
-        fit(data)
+
+        for i_feats in range(self.n_feats):
+            if i_feats == 0: data_trans = self.LearnedFunctions[i_feats](data[:, i_feats])
+            else:
+                data_trans_slice =self.LearnedFunctions[i_feats](data[:, i_feats])
+                data_trans = np.c_[data_trans, data_trans_slice]
+
+
+        return data_trans
+
+    def fit_transform(self, data):
+        self.fit(data)
         return transform(data)
 
     def invtransform(self, data):
-        return self.LearnedInvFunctions[0](data)
+        data = np.array(data)
+        if data.ndim ==1 : data = data.reshape(-1,1)
+
+
+        for i_feats in range(self.n_feats):
+            if i_feats == 0: data_trans = self.LearnedInvFunctions[i_feats](data[:, i_feats])
+            else:
+                data_trans_slice =self.LearnedInvFunctions[i_feats](data[:, i_feats])
+                data_trans = np.c_[data_trans, data_trans_slice]
+
+
+        return data_trans
+
 
 
--- FunctionScaler/example.py
+++ FunctionScaler/example.py
@@ -2,14 +2,11 @@ from FunctionScaler import *
 import numpy as np
 import matplotlib.pyplot as plt
 
-mu, sigma  = 2., 0.5
 n_points = 100
-data = np.random.normal(mu, sigma, n_points)
 
-#count, bins, ignored = plt.hist(s, 30, normed=True)
-#plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - mu)**2 / (2 * sigma**2) ), linewidth=2, color='r')
-#plt.show()
 
+
+### Testing different ways of initialising FunctionScaler
 
 #fs = FunctionScaler("unif")  # calling the uniform function by name
 #fs = FunctionScaler("gauss") # calling the normal function by name 
@@ -17,25 +14,87 @@ data = np.random.normal(mu, sigma, n_points)
 fs = FunctionScaler(np.array(range(1,1+n_points))/(1.+n_points) ) # calling the uniform function directly by inv_cdf value
 
 
-fs.fit(data)
-data_transf = fs.transform(data)
-data_invtransf = fs.invtransform(data_transf)
+if False:
+    # Testing 1 D operation
+    mu, sigma  = 2., 0.5
+    data = np.random.normal(mu, sigma, n_points)
 
-#count, bins, ignored = plt.hist(data, 30, normed=True)
-#plt.hist(data_transf, 30, normed=True)
-#count, bins, ignored = plt.hist(data_invtransf, 30, normed=True)
-#plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - mu)**2 / (2 * sigma**2) ), linewidth=2, color='r')
+    fs.fit(data)
+    data_transf = fs.transform(data)
+    data_invtransf = fs.invtransform(data_transf)
+
+    if True:
+        # Testing standard 1 D operation
+        count, bins, ignored = plt.hist(data, 30, normed=True, alpha= 0.5, color="blue")
+        #plt.hist(data_transf, 30, normed=True)
+        plt.hist(data_invtransf, bins, normed=True, alpha = 0.5, color = "red")
+        #plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - mu)**2 / (2 * sigma**2) ), linewidth=2, color='r')
 
 
-extrapolated_data = [2, -4, 6]
-data_p_exp = np.append(extrapolated_data,data)
-data_p_exp_transf = fs.transform(data_p_exp)
-data_p_exp_invtransf = fs.invtransform(data_p_exp_transf)
 
-#count, bins, ignored = plt.hist(data_p_exp, 30, normed=True)
-#plt.hist(data_p_exp_transf, 30, normed=True)
-count, bins, ignored = plt.hist(data_p_exp_invtransf, 30, normed=True)
-#plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - mu)**2 / (2 * sigma**2) ), linewidth=2, color='r')
+    if False:
+        # Testing if data outside the trained range is handled correctly
+        extrapolated_data = [2, -4, 6]
+        data_p_exp = np.append(extrapolated_data,data)
+        data_p_exp_transf = fs.transform(data_p_exp)
+        data_p_exp_invtransf = fs.invtransform(data_p_exp_transf)
+
+        #count, bins, ignored = plt.hist(data_p_exp, 30, normed=True)
+        #plt.hist(data_p_exp_transf, 30, normed=True)
+        count, bins, ignored = plt.hist(data_p_exp_invtransf, 30, normed=True)
+        #plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - mu)**2 / (2 * sigma**2) ), linewidth=2, color='r')
+
+if True:
+    # Testing 1 D operation with 0 variance
+    mu, sigma  = 2., 0.
+    data = np.random.normal(mu, sigma, n_points-1)
+    data = np.append(data, 5)
+
+    fs.fit(data)
+    data_transf = fs.transform(data)
+    data_invtransf = fs.invtransform(data_transf)
+
+    if False:
+        # Testing standard 1 D operation
+        count, bins, ignored = plt.hist(data, 30, normed=True, alpha= 0.5, color="blue")
+        #plt.hist(data_transf, 30, normed=True)
+        plt.hist(data_invtransf, bins, normed=True, alpha = 0.5, color = "red")
+        #plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - mu)**2 / (2 * sigma**2) ), linewidth=2, color='r')
+
+
+
+    if True:
+        # Testing if data outside the trained range is handled correctly
+        extrapolated_data = [20]
+        print fs.transform(extrapolated_data)
+        data_p_exp = np.append(extrapolated_data,data)
+        data_p_exp_transf = fs.transform(data_p_exp)
+        data_p_exp_invtransf = fs.invtransform(data_p_exp_transf)
+
+        #count, bins, ignored = plt.hist(data_p_exp, 30, normed=True, alpha= 0.5, color="blue")
+        plt.hist(data_p_exp_transf, 30, normed=True)
+        #plt.hist(data_p_exp_invtransf, bins, normed=True, alpha = 0.5, color = "red")
+        #plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - mu)**2 / (2 * sigma**2) ), linewidth=2, color='r')
+
+
+if False:
+    # Testing 2 D operation
+    mu = [ 0, -5, 20]
+    sigma = [1, 2, 2]
+    data = np.random.normal(mu[0], sigma[0], n_points)
+    for i_feat in range(1,len(mu)): data = np.c_[data, np.random.normal(mu[i_feat], sigma[i_feat], n_points)]
+
+    fs.fit(data)
+    data_transf = fs.transform(data)
+    data_invtransf = fs.invtransform(data_transf)
+
+    i_plot = 2
+
+    #count, bins, ignored = plt.hist(data, 30, normed=True)
+    #plt.hist(data_transf, 30, normed=True)
+    plt.hist(data_invtransf, 30, normed=True)
+    #plt.plot(bins, 1/(sigma * np.sqrt(2 * np.pi)) * np.exp( - (bins - mu)**2 / (2 * sigma**2) ), linewidth=2, color='r')
+
+
 plt.show()
-
 
